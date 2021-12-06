c2fa6cbc0715d5cae3ecb45f3dcd43fad9bd6813
SergioGCH <721520@unizar.es>
2021-12-06 17:15:54 +0100

    Cambiada librería de creación de horario

--- src/pages/CustomTimeline.js
+++ src/pages/CustomTimeline.js
@@ -1,129 +1,46 @@
 import React, { Component } from "react";
-import moment from "moment";
+import { Inject, ScheduleComponent, Week, WorkWeek, ViewsDirective, ViewDirective, ICalendarExport, DragAndDrop} from "@syncfusion/ej2-react-schedule";
 
 
-import generateFakeData from "./generate-fake-data";
-
-import Timeline, {
-  TimelineHeaders,
-  SidebarHeader,
-  DateHeader
-} from "react-calendar-timeline/lib";
-
-var keys = {
-  groupIdKey: "id",
-  groupTitleKey: "title",
-  groupRightTitleKey: "rightTitle",
-  itemIdKey: "id",
-  itemTitleKey: "title",
-  itemDivTitleKey: "title",
-  itemGroupKey: "group",
-  itemTimeStartKey: "start",
-  itemTimeEndKey: "end",
-  groupLabelKey: "title"
-};
-
-const itemProperties = {
-  onDoubleClick: () => { console.log('You clicked double!') },
-  style: {
-    background: 'blue'
-  }
+const clickableButton = {
+  display: 'flex',
+  marginLeft:'70vh',
+  alignItems: 'center', 
+  justifyContent: 'center', 
+  backgroundColor: "#685cf4", 
+  color: 'whitesmoke', 
+  borderRadius: '6px',
+  width: '13%', 
+  height: '5%', 
 }
 
 const itemInit = new Date("November 22, 2021 00:00:00")
 const itemEnd = new Date("November 22, 2021 24:00:00")
-const groupHeight = 120
+
+
 export default class App extends Component {
   constructor(props) {
     super(props);
-
-    const groups = [{ id: 1, title: '8-9', height: groupHeight }, { id: 2, title: '9-10', height: groupHeight }, { id: 3, title: '10-11', height: groupHeight }, { id: 4, title: '11-12', height: groupHeight }, { id: 5, title: '12-13', height: groupHeight }, { id: 6, title: '13-14', height: groupHeight }, { id: 7, title: '14-15', height: groupHeight }, { id: 8, title: '15-16', height: groupHeight },{ id: 9, title: '16-17', height: groupHeight }, { id: 10, title: '17-18', height: groupHeight }, { id: 11, title: '18-19', height: groupHeight }, { id: 12, title: '19-20', height: groupHeight }, { id: 13, title: '20-21', height: groupHeight }]
-    const items = [{ id: 1, title: 'Gestión de proyecto software', group: 2, start: itemInit, end: itemEnd, canResize: false, canChangeGroup: true, itemProps: itemProperties},{ id: 2, title: 'Laboratorio IS', group: 3, start: itemInit, end: itemEnd, canResize: false, canChangeGroup: true, itemProps: itemProperties}]
-    //const { groups, items } = generateFakeData();
-    const defaultTimeStart = moment()
-      .startOf("day")
-      .toDate();
-    const defaultTimeEnd = moment()
-      .startOf("day")
-      .add(1, "day")
-      .toDate();
-
-    this.state = {
-      groups,
-      items,
-      defaultTimeStart,
-      defaultTimeEnd
-    };
+    const scheduleObj = null
   }
 
-  handleItemMove = (itemId, dragTime, newGroupOrder) => {
-    const { items, groups } = this.state;
-
-    const group = groups[newGroupOrder];
-
-    this.setState({
-      items: items.map(item =>
-        item.id === itemId
-          ? Object.assign({}, item, {
-              start: dragTime,
-              end: dragTime + (item.end - item.start),
-              group: group.id
-            })
-          : item
-      )
-    });
-
-  };
-
-  handleItemResize = (itemId, time, edge) => {
-    const { items } = this.state;
-
-    this.setState({
-      items: items.map(item =>
-        item.id === itemId
-          ? Object.assign({}, item, {
-              start: edge === "left" ? time : item.start,
-              end: edge === "left" ? item.end : time
-            })
-          : item
-      )
-    });
-
-  };
+  onExportClick() {    
+      this.scheduleObj.exportToICalendar();
+  }
 
   render() {
-    const { groups, items, defaultTimeStart, defaultTimeEnd } = this.state;
 
     return (
-      <Timeline
-        groups={groups}
-        items={items}
-        keys={keys}
-        fullUpdate
-        stackItems={true}
-        itemTouchSendsClick={false}
-        itemHeightRatio={1}
-        canMove={true}
-        canResize={false}
-        defaultTimeStart={new Date("November 22, 2021 00:00:00")}
-        defaultTimeEnd={new Date("November 28, 2021 00:00:00")}
-        visibleTimeStart={new Date("November 22, 2021 00:00:00")}
-        visibleTimeEnd={new Date("November 28, 2021 23:59:59")}
-        onItemMove={this.handleItemMove}
-        onItemResize={this.handleItemResize}
-        itemTimeStartKey={"start_time"}
-        lineHeight={10}
-      >
-        <TimelineHeaders className="sticky">
-          <SidebarHeader>
-            {({ getRootProps }) => {
-              return <div {...getRootProps()}>Hora</div>;
-            }}
-          </SidebarHeader>
-          <DateHeader unit="primaryHeader" />
-          <DateHeader />
-        </TimelineHeaders>
-      </Timeline>
+      <div>
+        <button onClick={this.onExportClick.bind(this)} style={clickableButton}> Exportar a iCalendar </button>
+        <ScheduleComponent currentView='WorkWeek' startHour='09:00' endHour='21:00' ref={(schedule) => this.scheduleObj = schedule} >
+          
+          <ViewsDirective>
+            <ViewDirective option='WorkWeek' />
+          </ViewsDirective>
+          <Inject services={[Week, WorkWeek, ICalendarExport, DragAndDrop]}/>
+        </ScheduleComponent>
+      </div>
     );
   }
 }
