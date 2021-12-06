6b001e3a6d8237ddb908b7cbb068c0f67bd9d928
SergioGCH <721520@unizar.es>
2021-12-06 15:50:22 +0100

    Alguna mejora del horario

--- src/pages/CustomTimeline.js
+++ src/pages/CustomTimeline.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import moment from "moment";
 
-import Timeline from "react-calendar-timeline";
 
 import generateFakeData from "./generate-fake-data";
+
+import Timeline, {
+  TimelineHeaders,
+  SidebarHeader,
+  DateHeader
+} from "react-calendar-timeline/lib";
 
 var keys = {
   groupIdKey: "id",
@@ -18,12 +23,23 @@ var keys = {
   groupLabelKey: "title"
 };
 
+const itemProperties = {
+  onDoubleClick: () => { console.log('You clicked double!') },
+  style: {
+    background: 'blue'
+  }
+}
+
+const itemInit = new Date("November 22, 2021 00:00:00")
+const itemEnd = new Date("November 22, 2021 24:00:00")
+const groupHeight = 120
 export default class App extends Component {
   constructor(props) {
     super(props);
 
-    const groups = [{ id: 1, title: '8-9' }, { id: 2, title: '9-10' }, { id: 3, title: '10-11' }, { id: 4, title: '11-12' }, { id: 5, title: '12-13' }, { id: 6, title: '13-14' }, { id: 7, title: '14-15' }, { id: 8, title: '15-16' },{ id: 9, title: '16-17' }, { id: 10, title: '17-18' }, { id: 11, title: '18-19' }, { id: 12, title: '19-20' }, { id: 13, title: '20-21' }]
-    const { items } = generateFakeData();
+    const groups = [{ id: 1, title: '8-9', height: groupHeight }, { id: 2, title: '9-10', height: groupHeight }, { id: 3, title: '10-11', height: groupHeight }, { id: 4, title: '11-12', height: groupHeight }, { id: 5, title: '12-13', height: groupHeight }, { id: 6, title: '13-14', height: groupHeight }, { id: 7, title: '14-15', height: groupHeight }, { id: 8, title: '15-16', height: groupHeight },{ id: 9, title: '16-17', height: groupHeight }, { id: 10, title: '17-18', height: groupHeight }, { id: 11, title: '18-19', height: groupHeight }, { id: 12, title: '19-20', height: groupHeight }, { id: 13, title: '20-21', height: groupHeight }]
+    const items = [{ id: 1, title: 'Gestión de proyecto software', group: 2, start: itemInit, end: itemEnd, canResize: false, canChangeGroup: true, itemProps: itemProperties},{ id: 2, title: 'Laboratorio IS', group: 3, start: itemInit, end: itemEnd, canResize: false, canChangeGroup: true, itemProps: itemProperties}]
+    //const { groups, items } = generateFakeData();
     const defaultTimeStart = moment()
       .startOf("day")
       .toDate();
@@ -57,7 +73,6 @@ export default class App extends Component {
       )
     });
 
-    console.log("Moved", itemId, dragTime, newGroupOrder);
   };
 
   handleItemResize = (itemId, time, edge) => {
@@ -74,7 +89,6 @@ export default class App extends Component {
       )
     });
 
-    console.log("Resized", itemId, time, edge);
   };
 
   render() {
@@ -86,18 +100,30 @@ export default class App extends Component {
         items={items}
         keys={keys}
         fullUpdate
+        stackItems={true}
         itemTouchSendsClick={false}
-        stackItems
-        itemHeightRatio={0.75}
+        itemHeightRatio={1}
         canMove={true}
-        canResize={"both"}
+        canResize={false}
         defaultTimeStart={new Date("November 22, 2021 00:00:00")}
         defaultTimeEnd={new Date("November 28, 2021 00:00:00")}
         visibleTimeStart={new Date("November 22, 2021 00:00:00")}
         visibleTimeEnd={new Date("November 28, 2021 23:59:59")}
         onItemMove={this.handleItemMove}
         onItemResize={this.handleItemResize}
-      />
+        itemTimeStartKey={"start_time"}
+        lineHeight={10}
+      >
+        <TimelineHeaders className="sticky">
+          <SidebarHeader>
+            {({ getRootProps }) => {
+              return <div {...getRootProps()}>Hora</div>;
+            }}
+          </SidebarHeader>
+          <DateHeader unit="primaryHeader" />
+          <DateHeader />
+        </TimelineHeaders>
+      </Timeline>
     );
   }
 }
